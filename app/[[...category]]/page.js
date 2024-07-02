d3b678cb80282fecd88736dcc1e130832e37688d
Ibrahim Kamay <ibrahimkamayweb@gmail.com>
2024-07-02 21:56:47 +0300

    added skeleton all page

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -2,15 +2,23 @@ import React from "react";
 import HomeContainer from "@/containers/home";
 import Movies from "@/mocks/movies.json";
 
-export default function Home({ params }) {
-  console.log(params);
-  let selectedCategory;
-  if(params.category?.length > 0) {
-    selectedCategory = true;
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
+export default async function Home({ params }) {
+  await delay(2000);
+
+  let selectedCategory;
+  if (params.category?.length > 0) {
+    selectedCategory = true;
   }
-  return <HomeContainer selectedCategory = { {
-    id: params.category?.[0] ?? "",
-    movies: selectedCategory ? Movies.results.slice(0,7) : [],
-  } } />;
+  return (
+    <HomeContainer
+      selectedCategory={{
+        id: params.category?.[0] ?? "",
+        movies: selectedCategory ? Movies.results.slice(0, 7) : [],
+      }}
+    />
+  );
 }
