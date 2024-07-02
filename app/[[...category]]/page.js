1e898e6255e289e7aa689e5527bb406295bb654f
Halil İbrahim Kamay <ibrahimkamayweb@gmail.com>
2024-07-02 15:58:52 +0300

    added category page #1

---
+++ app/[[...category]]/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HomeContainer from "@/containers/home";
+import Movies from "@/mocks/movies.json";
+
+export default function Home({ params }) {
+  console.log(params);
+  let selectedCategory;
+  if(params.category?.length > 0) {
+    selectedCategory = true;
+
+  }
+  return <HomeContainer selectedCategory = { {
+    id: params.category?.[0] ?? "",
+    movies: selectedCategory ? Movies.results.slice(0,7) : [],
+  } } />;
+}
--- containers/home/index.js
+++ containers/home/index.js
@@ -6,11 +6,18 @@ import { FeaturedMovie } from "@/components/featured-movie";
 import Categories from "@/components/categories";
 import MoviesSection from "@/components/movies-section";
 
-function HomeContainer() {
+function HomeContainer({selectedCategory}) {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      {!!selectedCategory.movies.length && (
+        <MoviesSection
+          title={Genres.genres.find(({ id }) => id === +selectedCategory.id)?.name}
+          movies={selectedCategory.movies.slice(1, 7)}
+        />
+      )}
+      
       <MoviesSection
         title="Popular Movies"
         movies={Movies.results.slice(1, 7)}
